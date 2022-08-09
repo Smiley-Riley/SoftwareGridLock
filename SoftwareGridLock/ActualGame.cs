62eda6792109c121eeec4ca243aba63ba92299a2
Riley Sumskas <riley.sumskas@stu.pacifichills.nsw.edu.au>
2022-08-09 11:19:35 +1000

    Got rid of classes

--- SoftwareGridLock/ActualGame.cs
+++ SoftwareGridLock/ActualGame.cs
@@ -40,7 +40,6 @@ namespace SoftwareGridLock
                 lines.Add(line); 
             }
 
-            Grid grid = Grid.LoadGame(lines); 
 
         }
 
@@ -51,84 +50,7 @@ namespace SoftwareGridLock
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Grid grid = new Grid();
-            //List<Block> blocks = Block.loadBlocks();
-            //MessageBox.Show(grid.blocks[0]);
         }
 
-        private void pictureBox5_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
-
-    class Grid
-    {
-        public static Grid LoadGame(List<string> lines)
-        {
-            GreenBlock green = GreenBlock.FromString(lines.First());
-            //MessageBox.Show("stub");
-            List<Block> blocks = Block.loadBlocks(lines.Skip(1));
-            return new Grid(green, blocks);
-        }
-
-        private GreenBlock green;
-        private List<Block> blocks;
-
-        public Grid(GreenBlock green, List<Block> blocks)
-        {
-            this.green = green;
-            this.blocks = blocks;
-        }
-    }
-    class Block
-    {
-        public static List<Block> loadBlocks(IEnumerable<String> blockRecords)
-        {
-            List<Block> blocks = new List<Block>();
-            foreach (string blockRecord in blockRecords) //same as i = 0; i < .length... repeats over list
-            {
-                blocks.Add(Block.fromString(blockRecord)); //adds Block objects to "blocks" list 
-            }
-
-            return blocks;
-        }
-
-        public static Block fromString(string blockRecord)
-        {
-            string[] fields = blockRecord.Split(','); //creates an array of 'substrings', distinguished by the commas. so the string "red,hori,3" turns into array {red, hori, 3}
-            string colour = fields[0]; //Sets the different aspects of blocks to be accessible
-            string orientation = fields[1];
-            int length = Convert.ToInt32(fields[2]);
-            int x = Convert.ToInt32(fields[3]);
-            int y = Convert.ToInt32(fields[4]);
-            return new Block(colour, orientation, length, x, y);
-        }
-
-        private string colour;
-        private string orientation;
-        private int length;
-        public static int x;
-        public static int y;
-
-        public Block(string colour, string orientation, int length, int x, int y)
-        {
-            this.colour = colour;
-            this.orientation = orientation;
-            this.length = length;
-            Block.x = x;
-            Block.y = y;
-        }
-    }
-
-    class GreenBlock : Block
-    {
-        public static GreenBlock FromString(String record)
-        {
-            return new GreenBlock(x, y); //Relates to the Block.x and Block.y
-        }
-
-        public GreenBlock(int x, int y) : base("green", "-", 0, x, y)
-        { }
     }
 }
